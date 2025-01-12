a744e48c1bbb324f732fd77668742b9b115f0795
Marlon Villalona <iamvillalona@gmail.com>
2025-01-12 03:25:50 -0400

    feature: add new model UpdateTicketModel

---
+++ Models/UpdateTicketModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+
+namespace VipeSystem.Models
+{
+    public partial class UpdateTicketModel : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Tickets", "AssignedTo", c => c.Int(nullable: true));
+            AddColumn("dbo.Tickets", "CategoryId", c => c.Int(nullable: true));
+            // Otros cambios
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.Tickets", "AssignedTo");
+            DropColumn("dbo.Tickets", "CategoryId");
+            // Otros cambios
+        }
+    }
+
+}
